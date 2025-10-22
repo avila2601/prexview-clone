ba61e7f9adb58dda562e3ab6f3996f37d0d95295
avila2601 <diego2601@gmail.com>
2025-10-22 11:43:10 -0500

    plantillaIndep

---
+++ src/app/core/templates/invoice-template.ts
@@ -0,0 +1,108 @@
+export const InvoiceTemplate = {
+  header: `{{#with invoice}}
+<div class="row header">
+	<div class="col-3 relative">
+		<div class="logo">
+			<div class="box">
+				<div class="box">
+					{{$icon
+						"party_mode"
+						library="material-design"
+						width=80
+						height=80
+						fit=true
+						color="#fff"
+					}}
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-4 text-center"></div>
+	<div class="col-5">
+		<div class="details">
+			<h1>Invoice</h1>
+			<hr />
+			<div class="row">
+				<div class="col-6">
+					<div class="title"><b>Date issued</b></div>
+					<div class="block">{{$date _date_issued}}</div>
+				</div>
+				<div class="col-6">
+					<div class="title"><b>Invoice number</b></div>
+					<div class="block">{{_number}}</div>
+				</div>
+				<div class="col-12">
+					<div class="title"><b>Bill to</b></div>
+					<div class="block">{{bill_to._name}} ({{bill_to._email}})</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+{{/with}}`,
+
+  body: `{{#with invoice}}
+<div class="body">
+	<table>
+		<thead>
+			<tr>
+				<th class="text-left">ID</th>
+				<th class="text-left">Description</th>
+				<th class="text-right">Price</th>
+				<th class="text-right">Quantity</th>
+				<th class="text-right">Total</th>
+			</tr>
+		</thead>
+		<tbody>
+			{{#each order.product}}
+			<tr>
+				<td>{{_id}}</td>
+				<td>
+					<b>{{_name}}</b>
+					<br />
+					<span>{{_description}}</span>
+				</td>
+				<td class="text-right">$ {{$currency _price}}</td>
+				<td class="text-right">{{_quantity}}</td>
+				<td class="text-right">$ {{$currency _total}}</td>
+			</tr>
+			{{/each}}
+		</tbody>
+	</table>
+</div>
+{{/with}}`,
+
+  footer: `{{#with invoice}}
+<div class="footer">
+	<div class="row">
+		<div class="col-8">
+			<div class="thanks">
+				Thanks!
+			</div>
+			<br />
+			<div class="row">
+				<div class="col-12">
+					<div class="block"><b>Payment instructions</b></div>
+					<div class="block">{{payment_instructions}}</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-4">
+			<div class="total">
+				<div class="box box-left">
+					<h1>$ {{$currency order._subtotal}}</h1>
+				</div>
+				<div class="box box-right">
+					<h2>TOTAL</h2>
+					<h1>$ {{$currency order._total}}</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+{{/with}}`,
+
+  pagination: `<div class="pagination">
+	Page {{_page}} of {{_pages}}
+</div>`
+};
